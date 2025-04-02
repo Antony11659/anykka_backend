f3d37e2963f85f883067002e863b763712da2c87
Anton Shurupov <ant_shur@Antons-MacBook-Air.local>
2025-04-02 20:31:56 +0300

    set up a very simple sqlite db

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
-import Fastify from 'fastify'
+import Database from 'better-sqlite3';
 
-const fastify = Fastify({
-    logger: true
-  })
+const db = new Database('chinese_learning.db');
+
+db.pragma('foreign_keys = ON');
+
+async function getAllWords() {
+  try {
+      const stmt = db.prepare('SELECT * FROM words');
+      const words = stmt.all();
+      
+      console.log('Found words:', words.length);
+      console.table(words); // Nicely formatted output
+      
+      return words;
+  } catch (error) {
+      console.error('Error fetching words:', error);
+      throw error;
+  }
+}
+
+getAllWords();
 
 
-  // Declare a route
-fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' })
-  })
+
+
+
+// import Fastify from 'fastify'
+
+// const fastify = Fastify({
+//     logger: true
+//   })
+
+
+//   // Declare a route
+// fastify.get('/', function (request, reply) {
+//     reply.send({ hello: 'world' })
+//   })
   
 
-  // Run the server!
-  fastify.listen({ port: 3001 }, function (err, address) {
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
-    // Server is now listening on ${address}
-  })
+//   // Run the server!
+//   fastify.listen({ port: 3001 }, function (err, address) {
+//     if (err) {
+//       fastify.log.error(err)
+//       process.exit(1)
+//     }
+//     // Server is now listening on ${address}
+//   })
